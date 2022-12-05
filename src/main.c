8be99c127690291d6439c8c59567531eb811a74a
Ethan Page <ethanriverpage@gmail.com>
2022-12-05 11:33:05 -0500

    Let's try this

--- src/main.c
+++ src/main.c
@@ -4,16 +4,51 @@
 
 extern const hUGESong_t song;
 
+UBYTE joy;
+UBYTE c0 = 0, c1 = 0, c2 = 0, c3 = 0;
+
 void main()
 {
     NR52_REG = 0x80;
     NR51_REG = 0xFF;
     NR50_REG = 0x77;
 
-    hUGE_init(&song);
+    __critical
+    {
+        hUGE_init(&song);
+        add_VBL(hUGE_dosound);
+    }
+
     while (1)
     {
-        hUGE_dosound;
-        printf("Hello World");
+        wait_vbl_done();
+        joy = joypad();
+        switch (joy)
+        {
+        case J_UP:
+            c0 ^= 1;
+            hUGE_mute_channel(HT_CH1, c0);
+            set_bkg_tiles(0, 0, 1, 1, &map[c0 & 1]);
+            waitpadup();
+            break;
+        case J_DOWN:
+            c1 ^= 1;
+            hUGE_mute_channel(HT_CH2, c1);
+            set_bkg_tiles(1, 0, 1, 1, &map[c1 & 1]);
+            waitpadup();
+            break;
+        case J_LEFT:
+            c2 ^= 1;
+            hUGE_mute_channel(HT_CH3, c2);
+            set_bkg_tiles(2, 0, 1, 1, &map[c2 & 1]);
+            waitpadup();
+            break;
+        case J_RIGHT:
+            c3 ^= 1;
+            hUGE_mute_channel(HT_CH4, c3);
+            set_bkg_tiles(3, 0, 1, 1, &map[c3 & 1]);
+            waitpadup();
+            break;
+        }
     }
 }
