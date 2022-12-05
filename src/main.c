4dbbf1c01293732571d205741cbd477b87b8de7f
Ethan Page <ethanriverpage@gmail.com>
2022-12-05 11:15:01 -0500

    Music test

--- src/main.c
+++ src/main.c
@@ -1,10 +1,13 @@
 #include <gb/gb.h>
 #include <stdio.h>
+#include "hUGEDriver.h"
 
 void main()
 {
     while (1)
     {
+        extern const hUGESong_t song;
+        hUGE_init(&song);
         printf("Hello World");
     }
 }
