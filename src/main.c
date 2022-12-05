48f713ea3ca835f379edb36a55235f6a5ceb0257
Ethan Page <ethanriverpage@gmail.com>
2022-12-05 11:28:50 -0500

    Music fixes

--- src/main.c
+++ src/main.c
@@ -2,9 +2,14 @@
 #include <stdio.h>
 #include "include/hUGEDriver.h"
 
+extern const hUGESong_t song;
+
 void main()
 {
-    extern const hUGESong_t song;
+    NR52_REG = 0x80;
+    NR51_REG = 0xFF;
+    NR50_REG = 0x77;
+
     hUGE_init(&song);
     while (1)
     {
